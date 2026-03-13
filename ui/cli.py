a61bb6a7af41eef926a0f617401c732c3780ab91
ColinH13 <117141355+ColinH13@users.noreply.github.com>
2026-03-13 01:52:16 -0700

    Update cli.py

--- ui/cli.py
+++ ui/cli.py
@@ -121,7 +121,7 @@ def run_scan():
                 color = Fore.MAGENTA
                 symbol = "?"
 
-            print(color + status.upper(), control_id, title)
+            print(symbol + color + status.upper(), control_id, title)
 
             for result in results:
                 result_status = result['status']
@@ -140,7 +140,7 @@ def run_scan():
                     color = Fore.MAGENTA
                     symbol = "?"
 
-                print(color + "        " + result_status + ": " + description)
+                print(symbol + color + "        " + result_status + ": " + description)
             print("\n")
 
 
